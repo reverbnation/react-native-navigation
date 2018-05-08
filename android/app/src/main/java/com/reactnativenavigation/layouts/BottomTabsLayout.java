b002e751b926fb184efa5a95ae3fd2dae6c314a8
Jordan Floyd <jfloyd@reverbnation.com>
2018-05-08 10:09:17 -0400

    Revert "Android - Fix crash caused by refrencing view id which doesn't yet exist."
    This reverts commit 7988f2a237fe61c40c71d88985a9700e698d4182.

--- android/app/src/main/java/com/reactnativenavigation/layouts/BottomTabsLayout.java
+++ android/app/src/main/java/com/reactnativenavigation/layouts/BottomTabsLayout.java
@@ -81,8 +81,8 @@ public class BottomTabsLayout extends BaseLayout implements AHBottomNavigation.O
         createSideMenu();
         createBottomTabs();
         addBottomTabs();
-        createOverlay();
         addScreenStacks();
+        createOverlay();
         createSnackbarContainer();
         showInitialScreenStack();
         setInitialTabIndex();
